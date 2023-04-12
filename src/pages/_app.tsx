eda27a0909f95359920ce34ee621ec7a4d8576c2
arthur <arthurmedeiros32@gmail.com>
2023-04-12 01:40:08 -0300

    feat: configure chakra ui and make progress on home screen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,27 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
 import "~/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Barlow } from "next/font/google";
+import { theme } from "~/styles/theme";
+
+const barlow = Barlow({
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ChakraProvider theme={theme}>
+      <SessionProvider session={session}>
+        <main className={barlow.className}>
+          <Component {...pageProps} />
+        </main>
+      </SessionProvider>
+    </ChakraProvider>
   );
 };
 
